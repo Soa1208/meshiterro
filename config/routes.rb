9b989d430c6722f72f89e3e9e188582adf88cb8e
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-16 11:16:30 +0900

    [13章] 投稿者による削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/top'
   
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
   get 'homes/about', to: 'homes#about', as: 'about'
 end
