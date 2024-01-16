15ab445e5ba873b6c2a638970e4ff3acf01c43a1
Sota3110 <144866058+Soa1208@users.noreply.github.com>
2024-01-16 11:34:27 +0900

    [14章] usersコントローラーの作成 user.rbに追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get root to: "homes#top"
   devise_for :users
   get 'homes/top'
+  get 'homes/about', to: 'homes#about', as: 'about'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
-  get 'homes/about', to: 'homes#about', as: 'about'
+  resources :users, only: [:show, :edit]
 end
